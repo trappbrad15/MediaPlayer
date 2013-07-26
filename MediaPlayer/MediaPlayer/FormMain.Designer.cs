b33a940a6296b440682e2b3b29852cd1559c9080
Brad Trapp <bjthdc@gmail.com>
2013-07-25 19:46:44 -0500

    Renamed/working state

---
+++ MediaPlayer/MediaPlayer/FormMain.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace MediaPlayer
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelMedia = new System.Windows.Forms.Label();
+            this.textBoxMediaLocation = new System.Windows.Forms.TextBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.checkedListBoxMedia = new System.Windows.Forms.CheckedListBox();
+            this.comboBoxFileTypes = new System.Windows.Forms.ComboBox();
+            this.comboBoxFormats = new System.Windows.Forms.ComboBox();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelMedia
+            // 
+            this.labelMedia.AutoSize = true;
+            this.labelMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMedia.Location = new System.Drawing.Point(13, 13);
+            this.labelMedia.Name = "labelMedia";
+            this.labelMedia.Size = new System.Drawing.Size(135, 20);
+            this.labelMedia.TabIndex = 1;
+            this.labelMedia.Text = "Location of Media";
+            // 
+            // textBoxMediaLocation
+            // 
+            this.textBoxMediaLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxMediaLocation.Location = new System.Drawing.Point(154, 10);
+            this.textBoxMediaLocation.Name = "textBoxMediaLocation";
+            this.textBoxMediaLocation.Size = new System.Drawing.Size(317, 26);
+            this.textBoxMediaLocation.TabIndex = 2;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBrowse.Location = new System.Drawing.Point(491, 10);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 3;
+            this.buttonBrowse.Text = "Browse";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // checkedListBoxMedia
+            // 
+            this.checkedListBoxMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBoxMedia.FormattingEnabled = true;
+            this.checkedListBoxMedia.Location = new System.Drawing.Point(12, 106);
+            this.checkedListBoxMedia.Name = "checkedListBoxMedia";
+            this.checkedListBoxMedia.Size = new System.Drawing.Size(549, 310);
+            this.checkedListBoxMedia.TabIndex = 4;
+            // 
+            // comboBoxFileTypes
+            // 
+            this.comboBoxFileTypes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxFileTypes.FormattingEnabled = true;
+            this.comboBoxFileTypes.Location = new System.Drawing.Point(17, 49);
+            this.comboBoxFileTypes.Name = "comboBoxFileTypes";
+            this.comboBoxFileTypes.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxFileTypes.TabIndex = 5;
+            this.comboBoxFileTypes.SelectedIndexChanged += new System.EventHandler(this.comboBoxFileTypes_SelectedIndexChanged);
+            // 
+            // comboBoxFormats
+            // 
+            this.comboBoxFormats.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxFormats.FormattingEnabled = true;
+            this.comboBoxFormats.Location = new System.Drawing.Point(154, 49);
+            this.comboBoxFormats.Name = "comboBoxFormats";
+            this.comboBoxFormats.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxFormats.TabIndex = 6;
+            this.comboBoxFormats.SelectedIndexChanged += new System.EventHandler(this.comboBoxFormats_SelectedIndexChanged);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(404, 49);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(93, 36);
+            this.buttonPlay.TabIndex = 7;
+            this.buttonPlay.Text = "Play Media";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 467);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.comboBoxFormats);
+            this.Controls.Add(this.comboBoxFileTypes);
+            this.Controls.Add(this.checkedListBoxMedia);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.textBoxMediaLocation);
+            this.Controls.Add(this.labelMedia);
+            this.Name = "FormMain";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.SizeChanged += new System.EventHandler(this.FormMain_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMedia;
+        private System.Windows.Forms.TextBox textBoxMediaLocation;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.CheckedListBox checkedListBoxMedia;
+        private System.Windows.Forms.ComboBox comboBoxFileTypes;
+        private System.Windows.Forms.ComboBox comboBoxFormats;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MediaPlayer/MediaPlayer/FormMain.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MediaPlayer
+{
+    public partial class FormMain : Form
+    {
+        bool _loading = true;
+        bool _fileTypeChanging = false;
+        bool _mediaPlayerOpened = false;
+        string _mediaPath;
+        List<string> _mediaFiles;
+        List<string> _fileTypes;
+        List<string> videoTypes = new List<string>() { ".m4v", ".avi", ".XviD", ".mpg", ".flv" };
+        List<string> musicTypes = new List<string>() { ".m4a", ".mp3", ".flv", ".m4p" };
+        System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        AXVLC.VLCPlugin2Class mediaPlayer = new AXVLC.VLCPlugin2Class();
+        
+        public FormMain()
+        {
+            InitializeComponent();
+            _mediaFiles = new List<string>();
+            _fileTypes = new List<string>();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            textBoxMediaLocation.Text = ConfigurationManager.AppSettings["mediaLocation"];
+            comboBoxFileTypes.Text = ConfigurationManager.AppSettings["fileType"];
+            //Load ComboBox File Types
+            comboBoxFileTypes.Items.Add("Video");
+            comboBoxFileTypes.Items.Add("Music");
+
+            loadFileTypes();
+            //Load ComboBox Types
+            populateComboBoxTypes();
+
+            _mediaPath = textBoxMediaLocation.Text;
+            getFiles(_mediaPath);
+            _loading = false;
+        }
+
+        private void populateComboBoxTypes()
+        {
+            comboBoxFormats.Items.Clear();
+            comboBoxFormats.Items.Add("All Types");
+            foreach (string type in _fileTypes)
+            {
+                comboBoxFormats.Items.Add(type);
+            }
+            comboBoxFormats.Text = "All Types";
+        }
+
+        private void loadFileTypes()
+        {
+            if (comboBoxFileTypes.Text == "Video")
+            {
+                _fileTypes = videoTypes;
+            }
+            else
+            {
+                _fileTypes = musicTypes;
+            }
+        }
+
+        private void getFiles(string mediaPath)
+        {
+            foreach (string folder in Directory.GetDirectories(mediaPath))
+            {
+                getFiles(folder);
+                foreach (string file in Directory.GetFiles(folder))
+                {
+                    foreach (string fileType in _fileTypes)
+                    {
+                        if (file.EndsWith(fileType))
+                        {
+                            checkedListBoxMedia.Items.Add(file);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            folderBrowser.SelectedPath = textBoxMediaLocation.Text;
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                textBoxMediaLocation.Text = folderBrowser.SelectedPath;
+                _mediaPath = folderBrowser.SelectedPath;
+                checkedListBoxMedia.Items.Clear();
+                getFiles(_mediaPath);
+            }
+        }
+
+        private void FormMain_SizeChanged(object sender, EventArgs e)
+        {
+            checkedListBoxMedia.Size = new System.Drawing.Size(this.Width - 50, checkedListBoxMedia.Height);
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ConfigurationManager.AppSettings["fileType"] = comboBoxFileTypes.Text;
+            ConfigurationManager.AppSettings["mediaLocation"] = textBoxMediaLocation.Text;
+            config.Save();
+        }
+
+        private void comboBoxFormats_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!_loading && !_fileTypeChanging)
+            {
+                _fileTypes = new List<string>() { comboBoxFormats.Text };
+                checkedListBoxMedia.Items.Clear();
+                getFiles(_mediaPath);
+                _fileTypeChanging = false;
+            }
+        }
+
+        private void comboBoxFileTypes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!_loading)
+            {
+                _fileTypeChanging = true;
+                loadFileTypes();
+                populateComboBoxTypes();
+                checkedListBoxMedia.Items.Clear();
+                getFiles(_mediaPath);
+                _fileTypeChanging = false;
+            }
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+
+            foreach (var mediaFile in checkedListBoxMedia.CheckedItems)
+            {
+                mediaPlayer.addTarget(mediaFile.ToString(), new object(), AXVLC.VLCPlaylistMode.VLCPlayListInsert, 0);
+            }
+            if (!_mediaPlayerOpened)
+            {
+                mediaPlayer.play();
+                _mediaPlayerOpened = true;
+            }
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (_mediaPlayerOpened)
+            {
+                if (!mediaPlayer.playlist.isPlaying)
+                {
+                    mediaPlayer.playlistNext();
+                }
+            }
+        }
+    }
+}
